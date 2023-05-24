770187f49a17062b1599c2265ca30fed656936d7
Sherlin Sharmi <42143374+shearusherly@users.noreply.github.com>
2023-05-24 17:17:29 +0530

    Updated Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello!!! It's Shashwat Kumar.");
+		System.out.println("Hello!!! It's Sherlin Sharmi.");
 	}
 
 }
