14a3a506e9d604f5741cba89cbf403d81c0697f0
Sherlin Sharmi <42143374+shearusherly@users.noreply.github.com>
2023-05-25 09:40:06 +0530

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,8 @@
 public class Main {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
+
 		System.out.println("Hello!!! It's Sherlin here.....");
-=======
-		System.out.println("Hello!!! It's Sherlin Sharmi.");
->>>>>>> 770187f49a17062b1599c2265ca30fed656936d7
 	}
 
 }
